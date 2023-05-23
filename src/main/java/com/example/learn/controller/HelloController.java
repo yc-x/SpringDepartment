4e0d54d9bf635265336d1319091b9c03d246ad85
yc-x <34796396+yc-x@users.noreply.github.com>
2023-05-23 16:48:28 -0700

    Modify Hello Message

--- src/main/java/com/example/learn/controller/HelloController.java
+++ src/main/java/com/example/learn/controller/HelloController.java
@@ -1,11 +1,14 @@
 package com.example.learn.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${welcome.message}")
+    private String welcomeMessage;
     @GetMapping("/")
     public String helloWorld(){
         return "Hello, World!";
